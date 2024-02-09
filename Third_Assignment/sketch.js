4754e1cefc342d690dd964cf4e3edd6cdc397261
GusGt <gustavocoloma2024@gmail.com>
2024-02-08 18:43:41 -0600

    Update sketch.js

--- Third_Assignment/sketch.js
+++ Third_Assignment/sketch.js
@@ -3,25 +3,57 @@ let pos;
 
 function preload(){
 
-  spelunky = loadImage("Assets/Sprite_Stand.png");
+  //spl = loadAni('Assets/Sprite_Stand')
+  //spelunky = new sprite(10,10,80,80);
+  sheet = loadSpriteSheet("Assets/Sprite_Stand.png", 80, 80);
+  //los = loadImage("Assets/Sprite_Stand.png");
+  spelunky = loadAnimation();
+
+  spelunky.addAnis({
+
+    stand: {row: 0, frames: 1},
+    run: {row: 1, col: 1,frames: 8}
+
+  });
+
+  spelunky.changeAni('run');
 
 }
 
 function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  
-  image(spelunky,pos,0,5000,5000)
+	createCanvas(400,400);
+  //background('black')
 
 }
 
+function WalkU(){ //walk up 
+  spelunky.changeAni('stand')
+  spelunky.vel.y = 1;
+}
 
-function keyPressed(){
+function WalkD(){ //walk down
+  spelunky.changeAni('stand')
+}
 
-  if (value == "RIGHT"){
+function WalkR(){ //walk right
+  spelunky.changeAni('stand')
+}
 
-  }
+function WalkL(){ //walk left
+  spelunky.changeAni('stand')
+}
+
+function draw(){
+
+  clear();
+  animation(spelunky,80,80)
+
+  //image(los,0,0,80,80);
+
+  if(kb.presses('d')) WalkR()
+  if(kb.presses('a')) WalkL()
+  if(kb.presses('w')) WalkU()
+  if(kb.presses('s')) WalkS()
+
 
 }
