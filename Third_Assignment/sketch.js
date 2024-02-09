98a52880624b00eb91ff63d644a60b0edf0b49e0
GusGt <gustavocoloma2024@gmail.com>
2024-02-09 13:40:27 -0600

    sa
    as

--- Third_Assignment/sketch.js
+++ Third_Assignment/sketch.js
@@ -7,15 +7,15 @@ function preload(){
 
   let animations =  {
     stand: {row: 0, frames: 1},
-    run:  {row: 1, col: 1,frames: 8},
-    up:   {row:2,col:2,frames:8},
-    down: {row:3,col:3,frames:8}
+    run:  {row: 0, col: 1,frames: 8},
+    up:   {row:5,col:0,frames:6},
+    down: {row:5,col:7,frames:5}
   };
 
   spelunky.anis.frameDelay = 8;
-  spelunky.anis.offset.x = 2;
+
   spelunky.addAnis(animations);
-  spelunky.changeAni('run');
+  spelunky.changeAni('stand');
 
 }
 
@@ -27,31 +27,58 @@ function setup() {
 function draw(){
   clear();
   background(0);
-
-  spelunky.x = 200
-  spelunky.y = 200
  
-  // if(kb.presses('d')) WalkR()
-  // if(kb.presses('a')) WalkL()
-  // if(kb.presses('w')) WalkU()
-  // if(kb.presses('s')) WalkD()
+  if(kb.pressing('d')) WalkR()
+  else if(kb.pressing('a')) WalkL()
+  else if(kb.pressing('w')) WalkU()
+  else if(kb.pressing('s')) WalkD()
+  else Stop()
 
+
+  if(spelunky.x + 20 > width){
+    WalkL();
+  }
+  else if(spelunky.x - 20 < 0){
+    WalkR();
+  }
+  else if(spelunky.y - 20 < 0)
+  {
+    WalkD();
+  }
+  else if (spelunky.y  + 20 > height){
+    WalkU();
+  }
 
 }
 
-// function WalkU(){ //walk up 
-//   spelunky.changeAni('stand')
-//   //spelunky.vel.y = 1;
-// }
 
-// function WalkD(){ //walk down
-//   spelunky.changeAni('stand')
-// }
 
-// function WalkR(){ //walk right
-//   spelunky.changeAni('stand')
-// }
 
-// function WalkL(){ //walk left
-//   spelunky.changeAni('stand')
-// }
+
+function WalkU(){ //walk up 
+  spelunky.changeAni('up')
+  spelunky.vel.y = -1;
+}
+
+function WalkD(){ //walk down
+  spelunky.changeAni('down')
+  spelunky.vel.y = 1;
+}
+
+function WalkR(){ //walk right
+  spelunky.changeAni('run')
+  spelunky.scale.x = 1;
+  spelunky.vel.x = 1;
+}
+
+function WalkL(){ //walk left
+  spelunky.changeAni('run')
+  spelunky.scale.x = -1;
+  spelunky.vel.x = -1;
+}
+
+function Stop(){
+  spelunky.changeAni('stand')
+  spelunky.vel.x = 0;
+  spelunky.vel.y = 0;
+}
--- test/sketch.js
+++ test/sketch.js
@@ -1,31 +1,19 @@
-let hero;
+let sprite;
 
-function preload() {
-	hero = new Sprite(62, 24, 32, 32);
-	hero.spriteSheet = 'Assets/questKid.png';
-	hero.anis.offset.x = 2;
-	hero.anis.frameDelay = 8;
+function setup(){
 
-	hero.addAnis({
-		run: { row: 0, frames: 8 },
-		jump: { row: 1, frames: 6 },
-		roll: { row: 2, frames: 5, frameDelay: 14 },
-		turn: { row: 3, frames: 7 },
-		stand: { row: 3, frames: 1 }
-	});
-	hero.changeAni('run');
+	createCanvas(400,400);
+	sprite = new Sprite(200,200);
+	sprite.color = 'blue';
+
+
+
 }
 
-function setup() {
-	new Canvas(124, 48, 'pixelated x4');
-	allSprites.pixelPerfect = true;
-}
 
-function draw() {
-	clear();
-	if (kb.presses('r')) hero.changeAni('run');
-	if (kb.presses('j')) hero.changeAni('jump');
-	if (kb.presses('l')) hero.changeAni('roll');
-	if (kb.presses('t')) hero.changeAni('turn');
-	if (kb.presses('s')) hero.changeAni('stand');
+function draw(){
+
+	background(220);
+	sprite.rotation++;
+
 }
