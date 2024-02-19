b7b36b893e4d0d04d1f247fa9119c55d54577951
GusGt <gustavocoloma2024@gmail.com>
2024-02-19 12:51:35 -0600

    d

---
+++ Fourth_Assignment/sketch.js
@@ -0,0 +1,55 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+
+function bounds(spr){
+  if(spr.x + 20 > width){
+    WalkL(spr);
+  }
+  else if(spr.x - 20 < 0){
+    WalkR(spr);
+  }
+  else if(spr.y - 20 < 0)
+  {
+    WalkD(spr);
+  }
+  else if (spr.y  + 20 > height){
+    WalkU(spr);
+  }
+
+}  
+
+
+function WalkU(spr){ //walk up 
+  spr.changeAni('up')
+  spr.vel.y = -1;
+
+}
+
+function WalkD(spr){ //walk down
+  spr.changeAni('down')
+  spr.vel.y = 1;
+}
+
+function WalkR(spr){ //walk right
+  spr.changeAni('run')
+  spr.scale.x = 1;
+  spr.vel.x = 1;
+}
+
+function WalkL(spr){ //walk left
+  spr.changeAni('run')
+  spr.scale.x = -1;
+  spr.vel.x = -1;
+}
+
+function Stop(spr){
+  spr.changeAni('stand')
+  spr.vel.x = 0;
+  spr.vel.y = 0;
+}
