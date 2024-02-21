5c8edb910d302be08bbc6363745fb1f7d1744136
GusGt <gustavocoloma2024@gmail.com>
2024-02-20 20:35:16 -0600

    finished 8:35pm 2/20/24

--- Fourth_Assignment/sketch.js
+++ Fourth_Assignment/sketch.js
@@ -2,59 +2,128 @@ let sprite;
 let amount = [];
 let spritesheet;
 let vars = []
+let timeLeft = 30;
+let count = 25;
+let score = 0;
+let speed = 2;
+let startButton;
+let resetButton;
+let end = false;
+let start = false;
+
 
 function preload(){
 
-  let animations = {
-    run: {row:0 , frames: 2},
-    die: {row:1, frames: 1}
-  };
-
    spritesheet = loadImage("Assets/roach2.png")
-
- 
-   for(i = 0; i < 4; i++){
-  amount[i] = (new roach(random(100,1000),random(100,1000),100,100,spritesheet,animations));
-   }
-    // makes all roaches
-
 
 }
 
 function setup() {
 
-  createCanvas(1024,1024);
-  vars = ["WalkUp","WalkDown"];
+  createCanvas(800,800);
+  
+  startButton = createButton('Start Game');
+  startButton.size(200,50)
+  startButton.position(width/2-100,height/2);
+
+  
+
+  
+  startButton.mousePressed(() => {
+    
+    startButton.remove(); //remove button
+
+    
+  GameStart();
+  
+  amount.forEach((character) => {
+    
+    character.move();
+   
+  })
+  
+})
+
+
 }
 
+
+
 function mousePressed(){
-  amount[i].squished();
+  
+  for(let i = 0; i < count; i++){
+      let dead = amount[i].contains(mouseX, mouseY);
+      if(dead){
+        amount[i].squish();
+      }
+
+  }
+
+
+}
+
+function GameStart(){
+  start = true;
+  let animations = {
+    run: {row:0 , frames: 2},
+    die: {row:1, frames: 1}
+  };
+
+  for(i = 0; i < count; i++){
+    amount[i] = (new roach(random(1,800),random(1,800),100,100,spritesheet,animations));
+  }
+
+  
+
+}
+
+function time(){
+  if((frameCount % 60 == 0) && timeLeft >= 0){
+    timeLeft--;
+    // console.log(timeLeft);
+  }
+  return timeLeft;
 }
 
 function draw() {
-  background(235);
+  background('orange');
+  
+  if(!start){
+    textSize(30);
+    text("Welcome to Bug Squisher, your only goal,\n squash those bugs, your score depends on it.\n                             -_-", 100,200)
+  }
+  else if(start){
+    timeLeft = time();
+    textSize(30);
+    rect(0,1,180,80)
+    text("Score " + score,3,30);
+    text("Time Left: " + timeLeft,3,65);
+  }
 
+  if(timeLeft == 0) {
+    rectMode(CENTER);
+    square(width/2, height/2, 400);
+    textSize(30);
+    text("GAME OVER", width/2-80, height/2);
+    text("refresh to try again!", width/2-80, height/2+100); //for the life of me reset button would not work
+    text("You scored: "+ score , width/2-80, height/2 -45);
+    if(score == 0){
+      text("Really? Not even one?" , width/2-120, height/2 -85); //funny potential cue
+    }
+    end = true;
+    }
+    else if(score ==count){
+    rectMode(CENTER);
+    square(width/2, height/2, 400);
+    textSize(30);
+    text("ALL ENEMIES CRUSHED", width/2-175, height/2);
+    text("refresh to try again!", width/2-80, height/2+100);
+    text("You scored: "+ score , width/2-80, height/2 -45);
+    end = true;
+    }
+    
   
   amount.forEach((character) => {
-
-    
-    if (kb.pressing('d')) {
-      character.walkRight();
-    } 
-    else if (kb.pressing('a')) {
-      character.walkLeft();
-    } 
-    else if (kb.pressing('w')) {
-      character.walkUp();
-    }
-    else if (kb.pressing('s')) {
-      character.walkDown();
-    }
-    else {
-      //character.stop();
-      //character.walkRight();
-    }
-    
 
     if (character.sprite.x + 20 > width) {
       character.walkLeft();
@@ -66,13 +135,14 @@ function draw() {
       character.walkDown();
     }
     else if (character.sprite.y + 20 > height){
-      character.WalkUp();
+      character.walkUp();
     }
-    // else if(character.conatins(mouseX,mouseY)){
-    //    character.squished()
-    // }
+    else if(end ){
+      character.remove();
+    }
 
   })
+
 }
 
 
@@ -84,51 +154,77 @@ class roach {
     this.sprite.collider = 'none';
     this.sprite.addAnis(animations);
     this.sprite.changeAni('run');
+    this.checked = false;
     
 }
 
-  
-
-walkRight() {
-  //this.sprite.changeAni('walkRight');
-  this.sprite.vel.x = 1;
+  walkRight() {
+  this.sprite.vel.x = speed;
   this.sprite.scale.x = 1;
   this.sprite.vel.y = 0;
   this.sprite.rotateTo(90,25)
 }
 
 walkLeft() {
-  //this.sprite.changeAni('walkRight');
-  this.sprite.vel.x = -1;
+  this.sprite.vel.x = -speed;
   this.sprite.scale.x = -1;
   this.sprite.vel.y = 0;
   this.sprite.rotateTo(270,25)
 }
 
 walkUp() {
- // this.sprite.changeAni('walkUp');
-  this.sprite.vel.y = -1;
+  this.sprite.vel.y = -speed;
   this.sprite.vel.x = 0;
   this.sprite.rotateTo(0,25)
 }
 
 walkDown() {
- // this.sprite.changeAni('walkDown');
-  this.sprite.vel.y = 1;
+  this.sprite.vel.y = speed;
   this.sprite.vel.x = 0;
   this.sprite.rotateTo(180,25)
 }
 
-squished(){
+squish(){
+  this.sprite.vel.y = 0;
+  this.sprite.vel.x = 0;
+  this.sprite.changeAni('die')
 
-    this.sprite.changeAni("die")
+  if(!this.checked){
+    score++;
+    this.checked = true;
+    speed++;  //increases speed of each bug once hitting wall
+  }
 
 }
 
-spriteClicked() {
+stop(){
+  this.sprite.vel.y = 0;
+  this.sprite.vel.x = 0;
+}
 
-  if (this.contains(mouseX,mouseY))
-    this.remove();
+  contains(){
+
+        let insideX = mouseX >= this.sprite.x - 40 && mouseX <= this.sprite.x + 40;
+        let insideY = mouseY >= this.sprite.y - 40 && mouseY <= this.sprite.y + 40;
+
+        return insideX && insideY;
   }
 
+  move(){
+
+    let num = Math.floor(Math.random() * 4) +1 ; //creates random movement
+
+    switch(num){
+      case 1: this.walkUp();
+        break;
+      case 2: this.walkDown();
+        break;
+      case 3: this.walkRight();
+        break;
+      case 4: this.walkLeft();
+        break;
+    }
+  }
+
+  
 }
