dc80d3d9af3b38b020ad2168c08c4fe4dcfcad1a
GusGt <gustavocoloma2024@gmail.com>
2024-03-04 19:39:10 -0600

    done

--- Sixth_Assignment/sketch.js
+++ Sixth_Assignment/sketch.js
@@ -1,4 +1,16 @@
+let distAmt = new Tone.Distortion(0.5);
+let chorus = new Tone.Chorus(4,2.5,0.5);
+let freeverb = new Tone.Freeverb();
 let synth = new Tone.PolySynth(Tone.Synth);
+let pitcher = new Tone.PitchShift();
+
+pitcher.pitch = 0;
+synth.connect(freeverb);
+freeverb.connect(distAmt);
+distAmt.connect(pitcher);
+pitcher.connect(chorus);
+chorus.toDestination();
+
 
 let notes = {
   'a' :'C4',
@@ -15,6 +27,24 @@ synth.toDestination();
 
 function setup() {
   createCanvas(400, 400);
+  background("yellow");
+  text("Pitch",70,212);
+  text("Distortion",50, 234);
+  text("Keys A-K in ascending order, headphone warning for high pitch\nand distortion", 20, 380);
+  textSize(18);
+  text("WELCOME TO MC SOMETHING PART TWO",20,70)
+
+  pitch = createSlider(-12, 12, 0, 0.1); //pitch down -12, pitch up 12, starting point is 0
+  pitch.position (100, 200);
+  pitch.mouseMoved(()=> pitcher.pitch = pitch.value());
+
+  dist = createSlider(0,1,0,0.1); //distortion
+  dist.position(100,220);
+  dist.mouseMoved(() => distAmt.distortion = dist.value());
+
+  // core = createSlider(0,1,0,0.1);
+  // core.position(100,240);
+  // core.mouseMoved(() => chorus.Chorus.value() = core.value()); not affecting anything
 
 }
 
@@ -29,5 +59,5 @@ function keyReleased(){
 }
 
 function draw() {
-  background(220);
+  //background(220);
 }
