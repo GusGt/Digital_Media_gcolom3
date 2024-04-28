9a2801796cd0dbd8b879a535ad27ca042aa3a52f
GusGt <gustavocoloma2024@gmail.com>
2024-04-28 04:39:27 -0500

    -2nd edition
    -added basic enemy ai
    -added death screen
    -retry button in the works
    -added hit delay
    -finished level 2
    -level 3 empty
    -added death to enemies when hit
    -player now takes damage

--- FinalGame/sketch.js
+++ FinalGame/sketch.js
@@ -1,21 +1,30 @@
 let ground, sensor;
 let jumpHeight = -30;
 let charSpeed = 5;
-let menu, caveBack, platform, lives, character, score;
-let attack, enemyAttack, enemy;
+let lives = 3;
+let menu, caveBack, platform, character, score;
+let attack, isAttacking, enemyAttack, enemy, enemyImg;
 let temp;
 let menuSong, endSong, gameSong;
 let gameEnd, started;
 let enemyKill, death;
 let ninja, damageTaken, enemyDrop, powerUp, attackImg;
 let levelOne, levelTwo, levelThree;
-let gunDelay = 3000
+let gunDelay = 3000, hitDelay = 3000;
 let lastFire = 0;
+let lastHit = 0;
 let coin, coinImg, buttonImg1, buttonImg2;
 let font2, startButton, startButton2, retryButton;
 let currentlevel = 0;
 let changedOnce, changedTwice, changedThrice = false;
 let right = true;
+let curLev = 1;
+let sensor2_2, sensor2_1, sensor3_2, sensor3_1, sensor4_2, sensor4_1, sensor5_2, sensor5_1, sensor6_2, sensor6_1;
+let e2, e3, e4, e5, e6, e7, e8;
+let enemyList = [];
+let deathScreenOver, over, fadeIn, fade = 0, fadeAmount = 0;
+let isRetry = false;
+let animations;
 
 
 
@@ -36,6 +45,8 @@ function preload()
   menu = loadImage("Assets/mainMenu.png");
   font2 = loadFont("Assets/MedievalSharp.ttf");
   buttonImg1 = loadImage("Assets/startButton1.png");
+  enemyImg = loadImage("Assets/robot.png")
+  deathScreenOver = loadImage("Assets/deathScreen.png")
 
 }
 
@@ -45,10 +56,7 @@ function setup() {
   startButton.position(width/2-140, height/2);
   startButton.mousePressed(gameStart)
   
-
-}
-
-function draw() {
+  attack = new Sprite(-100, 0); //offset, an attack needs to be established somewhere to continue to levels
 
   background(menu);
   textSize(100);
@@ -57,12 +65,23 @@ function draw() {
   strokeWeight(5);
   textFont(font2);
   text("ESCAPE THE DUNGEON", width/2, height/2);
+}
+
+function draw() {
+
+  //console.log(mouseX, mouseY);
+  
   
   
   if(started)
   {
-    
-  background(caveBack);
+  
+    if(!over)
+    {
+      background(caveBack);
+      
+    }
+  
 
   if(kb.pressed('w')) jump(ninja)
   if(kb.pressing('d')) WalkR(ninja)
@@ -76,15 +95,83 @@ function draw() {
   else Stop(ninja)
   bounds(ninja); //always checking bounds
 
+  //start of death condition
+
+  if(lives==0 || ninja.y > height)
+  {
+    
+    gameOver();
+
+    
+  }
+
+  //end of death condition
+
   if(currentlevel == 0 && !changedOnce)
   {
-    if(ninja.x + 20 > width+10)
+    if(ninja.x + 20 > width+10 || kb.pressed('p'))
     {
       changedOnce = true;
       levelTwoStart();
     }
   }
+  else if(currentlevel == 1 && !changedTwice)
+  {
+    if(ninja.x + 20 > width+10 || kb.pressed('l'))
+    {
+      changedTwice = true;
+      levelThreeStart();
+    }
+  }
 
+  if(curLev == 2)
+  {
+    console.log(lives);
+    //console.log("here");
+
+    //start of enemy checks
+    if(!(sensor2_2.overlapping(platform)))
+    {
+      eWalkL(enemyList[0]);
+    }
+    else if(!(sensor2_1.overlapping(platform)))
+    {
+      eWalkR(enemyList[0]);
+    }
+    if(!(sensor3_2.overlapping(platform)))
+    {
+      eWalkL(enemyList[1]);
+    }
+    else if(!(sensor3_1.overlapping(platform)))
+    {
+      eWalkR(enemyList[1]);
+    }
+
+    //end of enemy checks
+    if(attack.overlapping(enemyList[0]) && isAttacking)
+    {
+      defeated(enemyList[0]);
+      score += 5;
+    }
+    else if(attack.overlapping(enemyList[1]) && isAttacking)
+    {
+      defeated(enemyList[1]);
+      score += 5;
+    }
+    else if(ninja.overlapping(enemyList[0]) && (millis() - lastHit > hitDelay))
+    {
+      lives--;
+      lastHit = millis();
+    }
+    else if(ninja.overlapping(enemyList[1]) && (millis() - lastHit > hitDelay))
+    {
+      lives--;
+      lastHit = millis();
+    }
+   
+  }
+
+ 
 
   }
   
@@ -97,8 +184,46 @@ function collect(spr, coin){
 
 }
 
+function gameOver(){
+  allSprites.remove();
+  over = true;
+  isRetry = true;
+
+    
+    if(fade<=255 && !fadeIn)
+    {
+      fade += 5;
+    }
+    if(fade>255 || fadeIn)
+    {
+      fadeIn = true;
+      fade += -1;
+      
+      if(fade < 200)
+      {
+        
+        fill('black');
+        rect(0,0,300,800);
+        rect(900,0,300,800);
+        background(deathScreenOver);
+        strokeWeight(5);
+        textFont(font2);
+        textSize(50);
+        fill('black');
+        textAlign(CENTER);
+        text("YOU HAVE BECOME LOST\nIN THE DUNGEON", width/2, height/2-200);
+        
+      }
+
+    }
+  fill(0,fade)
+  rect(0,0,1600,1000)
+  lives = 0;
+}
+
 function fire(spr){
 
+ isAttacking = true;
  attack = new Sprite(spr.x, spr.y);
  if(right)
  {
@@ -115,6 +240,9 @@ function fire(spr){
  attack.img = attackImg;
  attack.collider = 'none';
  attack.life = 80;
+ attack.onLifetimeEnd = function() { // Define function to be executed when attack's lifespan ends
+ isAttacking = false; // Set isAttacking to false
+};
 
 }
 
@@ -124,6 +252,25 @@ function bounds(spr){
     WalkR(spr);
   }
 } 
+
+//enemy controls start
+
+function eWalkR(spr){ //walk right
+  spr.changeAni('run')
+  spr.scale.x = 1;
+  spr.vel.x = 1;
+  
+}
+
+function eWalkL(spr){ //walk left
+  spr.changeAni('run')
+  spr.scale.x = -1;
+  spr.vel.x = -1;
+  
+  
+}
+
+//end of enemy controls
 
 function WalkR(spr){ //walk right
   spr.changeAni('run')
@@ -154,7 +301,37 @@ function Stop(spr){
   
 }
 
+function resetAll(){
+  
+  fade = 0;
+  fadeIn = false;
+  changedOnce = false;
+  changedTwice = false;
+  changedTwice = false;
+  lives = 3;
+  lastFire = 0;
+  lastHit = 0;
+  curLev = 1;
+  currentlevel = 0;
+  started = true;
+  over = false;
+  isRetry = false;
+}
+
+function defeated(spr){
+  spr.remove();
+}
+
 function gameStart(){
+  if(isRetry)
+  {
+    console.log("here2");
+    resetAll();
+    console.log(lives, "here3");
+    
+  }
+  
+  
   startButton.remove();
   started = true;
   
@@ -162,7 +339,7 @@ function gameStart(){
 
   ninja = new Sprite(30,570,80,80);
   ninja.spriteSheet = 'Assets/Ninja.png';
-  let animations =  {
+  animations =  {
     stand: {row: 0, frames: 1},
     run:  {row: 0, col: 1,frames: 8},
     jump: {row:9, col:0, frames:12}
@@ -201,7 +378,7 @@ function gameStart(){
   ninja.rotationLock = true;
   ninja.friction = 0;
 
-  sensor = new Sprite(30,595,30,95,'n');
+  sensor = new Sprite(30,595,60,55,'n');
   let e = new GlueJoint(ninja,sensor);
   e.visible = false;
   sensor.visible = false;
@@ -262,10 +439,117 @@ function gameStart(){
 }
 
 function levelTwoStart(){
+  curLev =2;
+  currentlevel = 1;
+  let animationsEn =  {
+    stand: {row: 0, frames: 1},
+    run:  {row: 0, col: 1,frames: 8}
+  };
+
   levelOne.remove();
+  
   ninja.x = 10;
   ninja.y = 50;
   levelTwo = new Tiles(
+		[
+			'                                                                        ',
+			'                                                                         -',
+			'                                                                        ',
+			'            c c c                                                        -',
+			'                                                                        ',
+			'                                                                         -',
+			'                                                                        ',
+			'-  -  -                                               -  -  -            - ',
+			'                                                                        ',
+      '                                                                         -   ',
+      '                                                                        ',
+      '                                                                         -  ',
+      '                                                                        ',
+      '                                                                        ',
+      '                                                                        ',
+      '                                                                        ',
+      '                                                                        ',
+      '           c     c                                                      ',
+      '                                                                        ',
+      '                                                                        ',
+      '        -  -  -  -  -                                                   ',
+      '                                                                         -  ',
+      '                                                                        ',
+      '                                                                         -  ',
+      '                                                                        ',
+      '                                                                         -  ',
+      '                                                             -  -  -    ',
+      '                                                                         -  ',
+      '                                                                        ',
+      '                                                                         -  ',
+      '                                                                        ',
+      '                                                                         -  ',
+      '                                               -  -  -  -               ',
+      '                                                                         -  ',
+      '                                                                        ',
+      '                                                                         -  ',
+      '                                                                        ',
+      '                                                                         -  ',
+			'  -  -  -                      -        -  -                            ',
+      '                                                                         -  ',
+      '                                                                        ',
+      '                                                                         -  ',
+      '                          c                                             ',
+      '                                                                         -  ',
+      '                                                      c  c              ',
+      '                                                                         -  ',
+      '                                                      -  -              ',
+      '             -       -  -                                                -  ',
+      '                                                                         ',
+      '                                                                         -  ',
+		],
+		8,
+		8,
+		16,
+		16
+	);
+
+  enemyList[0] = new Sprite(200,280,80,80);
+  enemyList[0].spriteSheet = "Assets/robot.png";
+  enemyList[0].addAnis(animationsEn);
+  enemyList[0].layer = 0;
+  enemyList[0].rotationLock = true;
+  enemyList[0].friction = 0;
+  enemyList[0].collider = 'none';
+  sensor2_1 = new Sprite(enemyList[0].x-15,enemyList[0].y+40,60,20,'n');
+  sensor2_2 = new Sprite(enemyList[0].x+15,enemyList[0].y+40,60,20,'n');
+  e2 = new GlueJoint(enemyList[0],sensor2_1);
+  e3 = new GlueJoint(enemyList[0],sensor2_2);
+  //sensor2_1.visible = false; sensor2_2.visible = false;
+  eWalkR(enemyList[0]);
+
+  enemyList[1] = new Sprite(810,470,80,80);
+  enemyList[1].spriteSheet = "Assets/robot.png";
+  enemyList[1].addAnis(animationsEn);
+  enemyList[1].layer = 0;
+  enemyList[1].rotationLock = true;
+  enemyList[1].friction = 0;
+  enemyList[1].collider = 'none';
+  sensor3_1 = new Sprite(enemyList[1].x-15,enemyList[1].y+40,60,20,'n');
+  sensor3_2 = new Sprite(enemyList[1].x+15,enemyList[1].y+40,60,20,'n');
+  e4 = new GlueJoint(enemyList[1],sensor3_1);
+  e5 = new GlueJoint(enemyList[1],sensor3_2);
+  //sensor3_1.visible = false; sensor3_2.visible = false;
+  eWalkL(enemyList[1])
+  
+
+
+
+}
+
+function levelThreeStart(){
+
+  levelTwo.remove();
+  enemyList[0].remove();
+  enemyList[1].remove();
+  ninja.x = 10;
+  ninja.y = 700;
+  levelThree = new Tiles(
 		[
 			'                                                                        ',
 			'                                                                        ',
@@ -274,7 +558,7 @@ function levelTwoStart(){
 			'                                                                        ',
 			'                                                                        ',
 			'                                                                        ',
-			'-  -  -                                                                 ',
+			'                                                                        ',
 			'                                                                        ',
       '                                                                        ',
       '                                                                        ',
@@ -305,7 +589,16 @@ function levelTwoStart(){
       '                                                                        ',
       '                                                                        ',
       '                                                                        ',
-			'                                                                        '
+			'                                                                        ',
+      '                                                                        ',
+      '                                                                        ',
+      '                                                                        ',
+      '                                                                        ',
+      '                                                                        ',
+      '                                                                        ',
+      '                                                                        ',
+      '                                                                        ',
+      ' -  -  -                                                                ',
 		],
 		8,
 		8,
@@ -314,4 +607,5 @@ function levelTwoStart(){
 	);
 
 }
+
 
